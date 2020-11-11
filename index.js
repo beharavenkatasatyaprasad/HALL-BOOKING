00c3a5ecbd9cb9af73bc8b63a40982c8920f1da0
Satya Prasad Behara <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-11-12 02:04:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
-const port = 3000 || process.env.PORT
+const port =process.env.PORT || 3000 
 app.use(express.json());
 
 app.get('/', (req,res) => {
