375f362bd929ca6426b42cdebdff6714878bea38
Satya Prasad Behara <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-11-12 01:59:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,126 +1,115 @@
-const Joi = require('joi');
 const express = require('express');
+const Joi = require('joi');
 const app = express();
-
+const port = 3000 || process.env.PORT
 app.use(express.json());
-const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log('listening on port' , `${port}`));
+app.get('/', (req,res) => {
+    res.redirect('/api')
+})
 
-app.get("/", function (req, res) {
+app.get('/api',(req,res) => {
     res.status(200)
     res.send(`
     <ul>
         <h1>Welcome</h1>
-        <li><h1>For Creating room use <span style="color:red">/CreateRoom</span></h1></li>
-        <li><h1>For Adding New Customer <span style="color:red">/addNewCustomer</span></h1></li>
-        <li><h1>For Booking room use <span style="color:red">/BookRoom</span></h1></li>
-        <li><h1>to List All rooms use <span style="color:red">/ListAllRooms</span></h1></li>
-        <li><h1>to List All Customers use <span style="color:red">/ListAllCustomers</span></h1></li>
+        <li><h1>For Creating room use <span style="color:red">/api/createRoom</span></h1></li>
+        <li><h1>For Booking room use <span style="color:red">/api/bookRoom</span></h1></li>
+        <li><h1>to List All rooms use <span style="color:red">/api/ListAllRooms</span></h1></li>
+        <li><h1>to List All Customers use <span style="color:red">/api/ListAllCustomers</span></h1></li>
     </ul>
     `);
-});
-  
+
+    res.end()
+})
+
 let customers = []
-
 let rooms = []
+let  customer_bookings = []
 
-let customer_bookings = []
-//Create a new room endpoint
+//create an endpoint for creating new room
+app.post('/api/createRoom', (req,res) => {
 
-app.post('/CreateRoom', (req, res) =>{
     const schema = Joi.object({
-        name : Joi.string().min(5).required(),
+        name : Joi.string().required(),
         amenities : Joi.array(),
-        location : Joi.string().required(),
+        NoOfSeatsAvail : Joi.number().required(),
         oneHourPrice : Joi.number().required()
-    });
-
+    });   
+   
     const {error} = schema.validate(req.body);
-    if(error) return res.send(error.details[0].message);
-
-    // Add the new entry in rooms
+    if(error){
+        return res.send(error.details[0].message);
+    }
 
     const newRoom = {
         id : rooms.length +1,
         name : req.body.name,
         amenities : req.body.amenities,
-        NoOfSeatsAvail : Joi.number().required(),
-        oneHourPrice : req.body.oneHourPrice
+        NoOfSeatsAvailable : req.body.NoOfSeatsAvail,
+        oneHourPrice : "Rupees "+req.body.oneHourPrice+"/- Only"
     };
-
     rooms.push(newRoom);
-    return res.status(201).json({ message : "Data successfully added", data : newRoom})
-});
+    return res.status(201).json({ message : "successfully Created New Room", data : newRoom})
+})
 
-//add new customer
-app.post('/addNewCustomer', (req, res) =>{
+//create an endpoint for Booking a room
+app.post("/api/bookRoom",(req,res) => {
     const schema = Joi.object({
-        name : Joi.string().min(5).required(),
-        address : Joi.string().required()
-    });
-
-    const {error} = schema.validate(req.body);
-    if(error) return res.send(error.details[0].message);
-
-    // Check if hall already Exists 
-    
-    let isPresent = customers.find((customer) => customer.name == req.body.name);
-    if(isPresent) return res.status(422).json({message : "customer with the same username already Exists"});
-
-    // Add the new entry in customers
-
-    const NewCustomer = {
-        id : customers.length +1,
-        name : req.body.name,
-        address : req.body.address
-        
-    };
-
-    customers.push(NewCustomer);
-    return res.status(201).json({ message : "Data successfully added", data : NewCustomer})
-});
-// Book room for customer
-
-app.post('/BookRoom', (req, res) => {
-    
-    const schema = Joi.object({
-        custid : Joi.number().required(),
+        CustomerName : Joi.string().required(),
         roomid : Joi.number().required(),
         startDate : Joi.date().required(),
         endDate : Joi.date().required()
     });
-
+   
     const {error} = schema.validate(req.body);
-    if(error) return res.status(400).json({message : "Bad request", data : error.details[0].message});
+    if(error){
+        return res.send(error.details[0].message);
+    }
 
-    // Check if the room is booked already in the same time range.
-
-    const bookingExists = customer_bookings.find((booking) =>
+        const bookingExists =  customer_bookings.find((booking) =>
             (((req.body.startDate <= booking.startDate && (req.body.endDate >= booking.startDate && req.body.endDate <= booking.endDate))
             || (req.body.endDate >= booking.startDate && req.body.endDate <= booking.endDate)))
             && booking.roomid== req.body.roomid);
     
-    if(bookingExists) return res.status(422).json({message : "There is already a booking available in the same time range", data : bookingExists});
+    if(bookingExists) return res.status(422).json({message : "Sorry, the room is already booked in the same time range"});
 
     let newBooking = {
-        id : customer_bookings.length + 1,
-        custid : req.body.custid,
+        Bookingid :  customer_bookings.length + 1,
         roomid : req.body.roomid,
         startDate : req.body.startDate,
         endDate : req.body.endDate
     }
-
+    const CustomerDetails = {
+        name : req.body.CustomerName,        
+        custid : customers.length + 1,
+    };
+    customers.push(CustomerDetails);
     customer_bookings.push(newBooking);
-    console.log(customer_bookings);
-    return res.status(201).json({message : "Booking successful..."})
-});
+    // console.log(customers)
+    return res.status(201).json({ message : "Booking Successful", BookingDetails : newBooking, customerDetails : CustomerDetails})
+})
 
-// List all rooms with booked data
+//create an endpoint for listing all rooms with booked data
+app.get("/api/listAllRooms",(req,res) =>{
+    const allRoomsData = [];
+    customer_bookings.forEach((booking) =>{
+        bookingViewModel = {
+            roomName : rooms.find(x => x.id == booking.roomid).name,
+            customerName : customers.find(x => x.id == booking.custid).name,
+            bookedStatus : "Successful",
+            startDate : booking.startDate,
+            endDate : booking.endDate
+        }
+        allRoomsData.push(bookingViewModel);
+    });
 
-app.get('/listAllRooms', (req, res) => {
-    let result = [];
-    let bookingViewModel;
+    res.send(allRoomsData);
+})
+
+//create an endpoint for listing all customers with booked data
+app.get("/api/listAllCustomers",(req,res) =>{
+    const allCustomersData = [];
     customer_bookings.forEach((booking) =>{
         bookingViewModel = {
             customerName : customers.find(x => x.id == booking.custid).name,
@@ -128,26 +117,13 @@ app.get('/listAllRooms', (req, res) => {
             startDate : booking.startDate,
             endDate : booking.endDate
         }
-        result.push(bookingViewModel);
+        allCustomersData.push(bookingViewModel);
     });
 
-    res.send(result);
-});
+    res.send(allCustomersData);
+})
 
-// List all customers
 
-app.get('/listAllCustomers', (req, res) => {
-    let result = [];
-    let customersViewModel;
-    customers.forEach((customer) =>{
-        customersViewModel = {
-            customerID : customer.id,
-            customerName : customer.name,
-            address : customer.address,
-            isActive : customer.isActive
-        }
-        result.push(customersViewModel);
-    });
-
-    res.send(result);
+app.listen(port,() =>{
+    console.log('listening on port' , `${port}`)
 });
